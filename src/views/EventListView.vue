d91f877edd6d19175a5adbbf89da52722bad1efa
Dmitry Kostyuk <dima.kostyuk1508@gmail.com>
2022-06-06 20:11:22 +0300

    L5 finish

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,6 +1,9 @@
 <template>
     <h1>Events For Good</h1>
-    <div class="events-list">
+    <div
+        v-if="events.length"
+        class="events-list"
+    >
         <EventCard
             v-for="event in events"
             :key="event.id"
@@ -11,6 +14,7 @@
 
 <script>
     import EventCard from '@/components/EventCard.vue';
+    import EventsService from '@/services/EventsService';
 
     export default {
         name: 'EventList',
@@ -21,42 +25,16 @@
 
         data() {
             return {
-                events: [
-                    {
-                        id: 5928101,
-                        category: 'animal welfare',
-                        title: 'Cat Adoption Day',
-                        description: 'Find your new feline friend at this event.',
-                        location: 'Meow Town',
-                        date: 'January 28, 2022',
-                        time: '12:00',
-                        petsAllowed: true,
-                        organizer: 'Kat Laydee'
-                    },
-                    {
-                        id: 4582797,
-                        category: 'food',
-                        title: 'Community Gardening',
-                        description: 'Join us as we tend to the community edible plants.',
-                        location: 'Flora City',
-                        date: 'March 14, 2022',
-                        time: '10:00',
-                        petsAllowed: true,
-                        organizer: 'Fern Pollin'
-                    },
-                    {
-                        id: 8419988,
-                        category: 'sustainability',
-                        title: 'Beach Cleanup',
-                        description: 'Help pick up trash along the shore.',
-                        location: 'Playa Del Carmen',
-                        date: 'July 22, 2022',
-                        time: '11:00',
-                        petsAllowed: false,
-                        organizer: 'Carey Wales'
-                    }
-                ]
+                events: []
             };
+        },
+
+        created() {
+            EventsService.getEvents()
+                .then(response => {
+                    this.events = response.data;
+                })
+                .catch(error => console.log(error));
         }
     };
 </script>
